bb91b25e5e43d3db61183480c39471094d6dbf1f
mariah2908 <mariah290807@gmail.com>
2026-06-16 15:27:23 -0300

    Delete autenticacaof.js

--- autenticacaof.js
+++
@@ -1,9 +0,0 @@
-function Entrar(){
-    let cpf=document.getElementById("txtcpf").Value
-    let senha=document.getElementById("txtsenha").Value
-
-    if(cpf==="" || senha===""){
-        alert("Preencha todos os campos!!!")
-        return false
-    }
-}
